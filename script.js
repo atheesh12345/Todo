23817d8424d51315d8716d96ea9c3cc22fd4abd8
atheesh12345 <132240162+atheesh12345@users.noreply.github.com>
2024-12-27 10:15:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello")
+let tasks = [];
+
+let taskform = document.getElementById("task-form");
+let tasklist = document.getElementById("list");
+
+
+taskform.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let title = document.getElementById("title").value;
+    let amount = document.getElementById("amount").value;
+    const task = {id:Date.now, title, amount, completed:false}
+    rendertask(task);
+})
+
+function rendertask(task){
+    const taskele =document.createElement("div");
+    taskele.innerHTML =`
+    <h3>${task.title}</h3>
+    <h3>${task.amount}</h3>`
+    tasklist.appendChild(taskele);
+}
